5ed6a4d34c4f278bf2cddd3bdd02938676e23f17
Kenny_G_Loggins <kenneth.t.smith@gmail.com>
2020-07-23 09:47:48 -0400

    # Uses logandpass.txt for user/pass list. Fixed: - added comments
    Todo:
    x add new users to txt file
    - terminate bad characters from user input
    - check length of user input
    - check strength of password
    
    Extra:
    - eventually learn to encrypt and turn this project into secure
    - change read function to read in chunks so this program can take a big list

--- login_non_secure.py
+++ login_non_secure.py
@@ -4,6 +4,8 @@
 # Project: Beginner_Loggin_non_secure
 
 # Purpose of this file is to practice my programing by making a login prompt.
+
+# Strips usernames separated by '=' from a file and stores them in list 'user_lst'
 def username_cache():
     user_lst = []
     with open('logandpass.txt', 'r') as login_and_password:
@@ -14,6 +16,8 @@ def username_cache():
                 user_lst.append(txt_key)
         return user_lst
 
+
+# Prompts user for a username, checks input against 'user_lst' by calling username_cache()
 def username():
     login_key = input('Please enter a Login Name: ')
     while username_cache().count(login_key) > 0:
@@ -21,6 +25,7 @@ def username():
     return login_key
 
 
+# Prompts user for a password and checks if they typed it in the same, twice
 def password():
     password1 = input('Please enter in a password: ')
     password2 = input('Please re-enter in a password: ')
@@ -29,8 +34,11 @@ def password():
     return password1
 
 
+# Runs both functions
 login = username()
 password = password()
 
+
+# Adds the valid username and password separated by '=' to 'logandpass.txt'
 with open('logandpass.txt', 'a+') as login_and_password:
         login_and_password.write(login + '=' + password + '\n')
